6be73a0f9a4b1e4635b7c4014f668f4da3e53fb3
Ronal David Valderrama Gomez <189985514+ronaldavidvalderrama@users.noreply.github.com>
2025-07-31 19:28:08 +0000

    feat: :sparkles: Rutas para autenticación y administración de usuarios

--- routes/user.php
+++ routes/user.php
@@ -1,13 +1,19 @@
 <?php
 
+#Rutas para autenticación y administración de usuarios.
+#Este archivo define las rutas para la autenticación de usuarios y administradores
+#dentro del grupo '/auth'. Utiliza el framework Slim para manejar solicitudes HTTP POST
+#relacionadas con acciones de registro e inicio de sesión
+
 use App\Controllers\UserController;
 use Slim\App;
 
 return function (App $app) {
     $app->group('/auth', function ($group) {
-        $group->post('/user', [UserController::class, 'createUser']);
-        $group->post('/user', [UserController::class, 'loginUser']);
-        $group->post('/admin', [UserController::class, 'createAdmin']);
-        $group->post('/admin', [UserController::class, 'loginAdmin']);
+        $group->post('/user/register', [UserController::class, 'createUser']);
+        $group->post('/user/login', [UserController::class, 'loginUser']);
+
+        $group->post('/admin/register', [UserController::class, 'createAdmin']);
+        $group->post('/admin/login', [UserController::class, 'loginAdmin']);
     });
 };
