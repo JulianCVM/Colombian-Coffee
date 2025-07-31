6fcb592c5fb5024e4ca3739f89ddcd9a9bd50591
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-07-31 02:10:08 +0000

    feat: :sparkles: Plantilla para el homepage, solo header

---
+++ frontend/frontend-react-colombian-coffee/src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import '../styles/HomePage.css';
+
+function HomePage() {
+    return (
+        <>
+        <div className='body-container'>
+            <div className="header-home">
+                <h1 className="header-title">Colombian Coffee</h1>
+                <p className="header-text">Catalogo de Variedades de Cafe en Colombia</p>
+            </div>
+            <div className="content-home">
+                
+            </div>
+        </div>
+        </>
+        
+    );
+}
+
+export default HomePage;
