f92b4df60f16be6f6821ab86ea7d41ea0d89a6a8
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-08-04 19:31:17 +0000

    feat: :sparkles: Se agregaron las rutas para los formularios

--- frontend/frontend-react-colombian-coffee/src/App.tsx
+++ frontend/frontend-react-colombian-coffee/src/App.tsx
@@ -6,6 +6,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AdminPanel from './pages/AdminPanel'
 import HistoriaForm from './pages/forms/HistoriaForm'
 import UbicacionForm from './pages/forms/UbicacionForm'
+import TamanhoGranoForm from './pages/forms/TamanhoGrano'
+import PorteForm from './pages/forms/PorteForm'
+import CondicionesForm from './pages/forms/CondicionesForm'
+import EnfermedadesForm from './pages/forms/EnfermedadForm'
+import DensidadForm from './pages/forms/DensidadForm'
+import CalidadGranoForm from './pages/forms/CalidadGranoForm'
+import PotencialForm from './pages/forms/PotencialRendimientoForm'
 
 function App() {
 
@@ -14,10 +21,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login></Login>}/>
-          <Route path='/Home' element={<HomePage></HomePage>}/>
-          <Route path='/Add' element={<AdminPanel></AdminPanel>} />
-          <Route path="/historia" element={<HistoriaForm />} />
-          <Route path="/ubicacion" element={<UbicacionForm />} />
+          <Route path='/home' element={<HomePage></HomePage>}/>
+          <Route path='/admin' element={<AdminPanel></AdminPanel>} />
+          <Route path="/admin/add/historia" element={<HistoriaForm />} />
+          <Route path="/admin/add/ubicacion" element={<UbicacionForm />} />
+          <Route path="/admin/add/tamanho-grano" element={<TamanhoGranoForm />} />
+          <Route path="/admin/add/porte" element={<PorteForm />} />
+          <Route path="/admin/add/condiciones" element={<CondicionesForm />} />
+          <Route path="/admin/add/enfermedad" element={<EnfermedadesForm />} />
+          <Route path="/admin/add/densidad" element={<DensidadForm />} />
+          <Route path="/admin/add/calidad-grano" element={<CalidadGranoForm />} />
+          <Route path="/admin/add/potencial-de-rendimiento" element={<PotencialForm />} />
         </Routes>
       </BrowserRouter>
     </>
